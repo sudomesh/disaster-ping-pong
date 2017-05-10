c2b9bd5f0481068e8a48fbce3000b62192c4d8d4
gcgallo <ggallo102@gmail.com>
2017-05-09 22:12:37 -0700

    ssid list alarm

--- init.lua
+++ init.lua
@@ -4,5 +4,10 @@ function listap(t)
     for k,v in pairs(t) do
         print(k.." : "..v)
     end
+    aplist = tmr.create()
+    aplist:register(5000, tmr.ALARM_SINGLE, function() wifi.sta.getap(listap) end) 
+    aplist:start()
 end
+
+wifi.setmode(wifi.STATIONAP)
 wifi.sta.getap(listap)
