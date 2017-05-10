7ee0cffa164c474a7df83b536401b7b660a1c93c
jhpoelen <jhpoelen@xs4all.nl>
2017-05-09 23:06:44 -0700

    because @gcgallo said so

--- init.lua
+++ init.lua
@@ -1,9 +1,22 @@
 
+-- toggle LED
+function toggleLED() 
+  gpio.mode(4, gpio.OUTPUT)
+  gpio.write(4, gpio.read(4) == gpio.HIGH and gpio.LOW or gpio.HIGH)
+end
+
 -- get all ssids
 function listap(t)
     for k,v in pairs(t) do
-	local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
-        print(k.." : "..rssi)
+	if string.find(k, "ESP") then 
+	  local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
+          print(k.." : "..rssi)
+	  blinky = tmr.create()
+	  blinky:register(20*rssi*rssi / rssi, tmr.ALARM_SINGLE, function() toggleLED end)
+	  tmr.start()
+	else 
+	  print("no ping, no pong")
+        end
     end
     aplist = tmr.create()
     aplist:register(5000, tmr.ALARM_SINGLE, function() wifi.sta.getap(listap) end) 
