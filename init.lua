d62416a581e0b5b35e7d68c0cbfa5f6462ac1941
jhpoelen <jhpoelen@xs4all.nl>
2017-05-10 00:21:07 -0700

    blinky

--- init.lua
+++ init.lua
@@ -7,21 +7,27 @@ end
 
 -- get all ssids
 function listap(t)
+    blinky:unregister()
     for k,v in pairs(t) do
 	if string.find(k, "ESP.*") then 
 	  local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
           print(k.." : "..rssi)
-	  blinky = tmr.create()
-	  blinky:register(20*rssi*rssi / rssi, tmr.ALARM_SINGLE, toggleLED)
-	  tmr.start()
-	else 
-	  print("no ping, no pong")
+          signal = -1*rssi < 20 and 20 or -1*rssi
+	  blinky:register(signal*signal/2, tmr.ALARM_AUTO, toggleLED)
+	  blinky:start()
+	--else 
+	  --print("no ping, no pong")
         end
     end
     aplist = tmr.create()
     aplist:register(5000, tmr.ALARM_SINGLE, function() wifi.sta.getap(listap) end) 
     aplist:start()
 end
+
+
+blinky = tmr.create()
+blinky:register(5000, tmr.ALARM_AUTO, toggleLED)
+blinky:start()
 
 wifi.setmode(wifi.STATIONAP)
 wifi.sta.getap(listap)
