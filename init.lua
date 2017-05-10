8aac6969c28f48ccbe103a794eeffa82ca9f08de
jhpoelen <jhpoelen@xs4all.nl>
2017-05-09 22:25:15 -0700

    add rssi

--- init.lua
+++ init.lua
@@ -2,12 +2,13 @@
 -- get all ssids
 function listap(t)
     for k,v in pairs(t) do
-        print(k.." : "..v)
+	local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
+        print(k.." : "..rssi)
     end
     aplist = tmr.create()
     aplist:register(5000, tmr.ALARM_SINGLE, function() wifi.sta.getap(listap) end) 
     aplist:start()
 end
 
-wifi.setmode(wifi.STATIONAP)
+wifi.setmode(wifi.STATION)
 wifi.sta.getap(listap)
